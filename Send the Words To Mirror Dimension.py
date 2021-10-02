8e79f9f34b92df7faf33cabf0c4b453ecc7f4523
shashi-gowda <91781996+shashi-gowda@users.noreply.github.com>
2021-10-02 22:06:27 +0530

    Update Send the Words To Mirror Dimension.py

--- Send the Words To Mirror Dimension.py
+++ Send the Words To Mirror Dimension.py
@@ -3,18 +3,3 @@ name=list(name)
 name.reverse()
 name=''.join(name)
 print(name)
-
-
-name=input("enter the string to reverse: ")
-name=list(name)
-name=name[::-1]
-name=''.join(name)
-print(name)
-
-
-
-string=(input('enter the string to be reversed: '))
-reverse=''
-for i in string:
-    reverse=i+reverse
-print(reverse)
