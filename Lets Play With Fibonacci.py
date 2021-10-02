de93d8f43b8c0aa0a27e9ffa25354d69bdcaca35
shashi-gowda <91781996+shashi-gowda@users.noreply.github.com>
2021-10-02 21:58:37 +0530

    Update Lets Play With Fibonacci.py

--- Lets Play With Fibonacci.py
+++ Lets Play With Fibonacci.py
@@ -5,20 +5,3 @@ for i in range(0,10):
     a=b
     b=c
     print(c, end=" ")
-
-
-
-x,y=0,1
-print(x, end=' ')
-while y<100:
-    print(y, end=' ')
-    x,y=y,x+y
-
-
-
-
-x,y=0,1
-print(x, end=' ')
-for i in range(10):
-    print(y, end=' ')
-    x,y=y,x+y
