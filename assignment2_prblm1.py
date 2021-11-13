5c11db704f08438c0397dca3766992f11378a0a4
shashi-gowda <91781996+shashi-gowda@users.noreply.github.com>
2021-11-13 22:18:16 +0530

    Update assignment2_prblm1.py

--- assignment2_prblm1.py
+++ assignment2_prblm1.py
@@ -1,9 +1,5 @@
 a=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-def last(n):
-    return n[-1]
-def sort(a):
-    return sorted(a,key=last)
-print('sorted:', sort(a))
-sorted: [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+b=sorted(a,key=last)
+print(b)
 
 
