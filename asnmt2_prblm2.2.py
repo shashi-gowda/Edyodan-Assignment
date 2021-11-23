fa4a6db8ae81095923db6d7559796ed630c424b7
shashi-gowda <91781996+shashi-gowda@users.noreply.github.com>
2021-11-23 18:44:12 +0530

    Update asnmt2_prblm2.2.py

--- asnmt2_prblm2.2.py
+++ asnmt2_prblm2.2.py
@@ -1,6 +1,6 @@
 small_alp=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 d={}
-for i in alp:
+for i in small_alp:
     d.update({i:ord(i)})
 print(d)
 
