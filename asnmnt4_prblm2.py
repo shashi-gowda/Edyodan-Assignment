d50a289113941bffd7e4041c6a9d2e292a23b24b
shashi-gowda <91781996+shashi-gowda@users.noreply.github.com>
2021-12-09 16:20:17 +0530

    Update asnmnt4_prblm2.py

--- asnmnt4_prblm2.py
+++ asnmnt4_prblm2.py
@@ -3,3 +3,17 @@
 sample_List=[1, 2, 3, 4, 5, 6, 7]
 qube_root=lambda x:x**3
 print(list(map(qube_root,sample_List)))
+
+
+
+
+
+
+
+
+# Write a Python program to triple all numbers of a given list of integers. Use Python map.
+
+def triple(l):
+    return l**3
+l=[1,2,4]
+a=list(map(triple,l))
