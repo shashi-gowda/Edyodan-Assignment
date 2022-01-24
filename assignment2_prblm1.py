fc995ce90a108a13f23a337c9ce798677763316a
shashi-gowda <91781996+shashi-gowda@users.noreply.github.com>
2022-01-24 19:56:47 +0530

    Update assignment2_prblm1.py

--- assignment2_prblm1.py
+++ assignment2_prblm1.py
@@ -3,4 +3,11 @@ a=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 b=sorted(a,key=itemgetter(1))
 print(b)
 
+or
+
+Sample=[(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+from operator import itemgetter
+Sample.sort(key=itemgetter(1))
+print(Sample)
+
 
